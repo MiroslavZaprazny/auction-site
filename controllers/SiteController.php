d15e1ea1a0574ce0ef71d4362113372aa7983c92
Mirek <miro.zaprazny8@gmail.com>
2021-12-15 20:47:44 +0100

    signup

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,6 +10,7 @@ use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
 use app\models\Auctions;
+use app\models\SignupForm;
 
 class SiteController extends Controller
 {
@@ -89,6 +90,19 @@ class SiteController extends Controller
         $model->password = '';
         return $this->render('login', [
             'model' => $model,
+        ]);
+    }
+
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            return $this->redirect(Yii::$app->homeUrl);
+        }
+
+        return $this->render('signup',[
+            'model'=> $model
         ]);
     }
 
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,6 +15,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -22,51 +23,53 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Aukcna Stranka',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top py-3',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav ms-auto'],
-        'items' => [
-            ['label' => 'Aukcie', 'url' => ['/site/index']],
-            ['label' => 'Predaj Auto', 'url' => ['/site/about']],
-            ['label' => 'Kontakt', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
-</header>
+    <header>
+        <?php
+        NavBar::begin([
+            'brandLabel' => 'Aukcna Stranka',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top py-3',
+            ],
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav ms-auto'],
+            'items' => [
+                ['label' => 'Aukcie', 'url' => ['/site/index']],
+                ['label' => 'Predaj Auto', 'url' => ['/site/about']],
+                ['label' => 'Kontakt', 'url' => ['/site/contact']],
+                Yii::$app->user->isGuest ? (['label' => 'Login', 'url' => ['/site/login']]
+                ) : ('<li>'
+                    . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+                    . Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>'
+                ),
+                Yii::$app->user->isGuest ? (['label' => 'Signup', 'url' => ['/site/signup']]
+                ) : ''
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+            ],
+        ]);
+        NavBar::end();
+        ?>
+    </header>
+
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
 
     <section class="p-5 " id='kontakt'>
         <div class="container">
@@ -77,21 +80,21 @@ AppAsset::register($this);
                     </h2>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">
-                        <span class="fw-bold">
-                            Adresa:
-                        </span>
+                            <span class="fw-bold">
+                                Adresa:
+                            </span>
                             Neviem 123
                         </li>
                         <li class="list-group-item">
-                        <span class="fw-bold">
-                            Email:
-                        </span>
+                            <span class="fw-bold">
+                                Email:
+                            </span>
                             email@email.com
                         </li>
                         <li class="list-group-item">
-                        <span class="fw-bold">
-                            Tel. cislo:
-                        </span>
+                            <span class="fw-bold">
+                                Tel. cislo:
+                            </span>
                             +421951351
                         </li>
                     </ul>
@@ -100,7 +103,8 @@ AppAsset::register($this);
         </div>
     </section>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
